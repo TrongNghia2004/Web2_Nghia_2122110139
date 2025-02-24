f8abf8a95efd946b1b86e5678f2cfbeef7b0be7c
Nghia20032004 <luongtrongnghia816@gmail.com>
2025-02-24 10:34:55 +0700

    first commit

---
+++ src/main/java/com/example/LuongTrongNghia/LuongTrongNghiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.LuongTrongNghia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuongTrongNghiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuongTrongNghiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LuongTrongNghia/LuongTrongNghiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LuongTrongNghia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuongTrongNghiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
